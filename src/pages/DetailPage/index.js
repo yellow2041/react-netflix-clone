340fc73a507f9e699207b08ef2834ac8e72899db
yellow2041 <jeeyeon654@gmail.com>
2024-02-18 23:11:43 +0900

    feat/ 검색결과 상세조회 페이지 개발

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from '../../api/axios';
 
 export default function DetailPage() {
-  return <div>DetailPage</div>;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`/movie/${movieId}`);
+      setMovie(request?.data);
+    }
+    fetchData();
+  }, []);
+  if (!movie) return <div>...loading</div>;
+  return (
+    <section>
+      <img
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt='poster'
+      />
+    </section>
+  );
 }
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from '../../api/axios';
 import './SearchPage.css';
 import { useDebounce } from '../../hooks/useDebounce';
 
 export default function SearchPage() {
+  const navigate = useNavigate();
   const [searchResults, setSearchResults] = useState([]);
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
@@ -20,7 +21,6 @@ export default function SearchPage() {
   }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async (searchTerm) => {
-    console.log(searchTerm);
     try {
       const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
       setSearchResults(request.data.results);
@@ -37,7 +37,12 @@ export default function SearchPage() {
             const movieImageUrl = 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path;
             return (
               <div key={movie.id} className='movie'>
-                <div className='movie__column-poster'>
+                <div
+                  onClick={() => navigate(`/${movie.id}`)}
+                  onKeyDown={() => navigate(`/${movie.id}`)}
+                  role='presentation'
+                  className='movie__column-poster'
+                >
                   <img src={movieImageUrl} alt='movie' className='movie__poster' />
                 </div>
               </div>
