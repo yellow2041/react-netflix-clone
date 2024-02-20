89dbd16effca327e3b8904a917e23e7116ce9c83
yellow2041 <jeeyeon654@gmail.com>
2024-02-20 17:55:10 +0900

    api key 제거

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://api.themoviedb.org/3',
   params: {
-    api_key: '730bf1f5e636e59914dcd72ec1352ba5',
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: 'ko-KR',
   },
 });
