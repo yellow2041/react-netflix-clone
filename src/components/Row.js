de2f85500d3bcc81da26c54ec1d8234b34a2a68c
yellow2041 <jeeyeon654@gmail.com>
2024-02-20 17:52:32 +0900

    feat/ row swipe 기능 적용

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,6 +2,12 @@ import axios from '../api/axios';
 import React, { useEffect, useState } from 'react';
 import './Row.css';
 import MovieModal from './MovieModal';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Row({ isLargeRow, fetchUrl, title, id }) {
   const [movies, setMovies] = useState([]);
@@ -16,10 +22,6 @@ export default function Row({ isLargeRow, fetchUrl, title, id }) {
     setMovies(request.data.results);
   };
 
-  const handleScroll = (isRight) => {
-    const move = isRight ? window.innerWidth - 80 : 80 - window.innerWidth;
-    document.getElementById(id).scrollLeft += move;
-  };
   const handleClick = (movie) => {
     setMovieSelected(movie);
     setModalOpen(true);
@@ -27,42 +29,35 @@ export default function Row({ isLargeRow, fetchUrl, title, id }) {
   return (
     <section className='row'>
       <h2>{title}</h2>
-      <div className='slider'>
-        <div className='slider__arrow-left'>
-          <span
-            className='arrow'
-            onClick={() => handleScroll(false)}
-            onKeyDown={() => handleScroll(false)}
-            role='presentation'
-          >
-            {'<'}
-          </span>
-        </div>
-
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: { slidePerView: 6, slidesPerGroup: 6 },
+          998: { slidesPerView: 5, slidesPerGroup: 5 },
+          625: { slidesPerView: 4, slidesPerGroup: 4 },
+          0: { slidesPerView: 3, slidesPerGroup: 3 },
+        }}
+      >
         <div id={id} className='row__posters'>
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-              alt={movie.name}
-              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-              onClick={() => handleClick(movie)}
-              onKeyDown={() => handleClick(movie)}
-              role='presentation'
-            />
+            <SwiperSlide key={movie.id}>
+              <img
+                className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+                style={{ padding: '25px 0' }}
+                alt={movie.name}
+                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                onClick={() => handleClick(movie)}
+                onKeyDown={() => handleClick(movie)}
+                role='presentation'
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className='slider__arrow-right'>
-          <span
-            className='arrow'
-            onClick={() => handleScroll(true)}
-            onKeyDown={() => handleScroll(true)}
-            role='presentation'
-          >
-            {'>'}
-          </span>
-        </div>
-      </div>
+      </Swiper>
+
       {modalOpen && <MovieModal {...movieSelected} setModalOpen={setModalOpen} />}
     </section>
   );
